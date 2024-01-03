6b6bb874dac8fe99ad74cd145c4be3230e16c9a8
Younes <75024641+younes-000@users.noreply.github.com>
2024-01-03 13:15:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tf-shared-sg-fncentral01-rg"
+    resource_group_name  = "rg-shared-mgmt-francecentral-001"
     storage_account_name = "sttfstatefncstg002"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
