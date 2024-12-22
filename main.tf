1a78164afa79a73eaf711e8b037df2d143f96c34
Younes <75024641+younes-000@users.noreply.github.com>
2024-12-22 19:04:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "rg-shared-mgmt-francecentral-001"
     storage_account_name = "sttfstatefncstg002"
     container_name       = "tfstate"
-    key                  = "HWlIALbCViCH9zCQr7v2eg6FDDxvrXEG13y/3XixOua9TvkWTUO8hQMcm5Wb6Uoi6WvnFRjd3DAq+AStWmWAXQ=="
+    key                  = ""
   }
 
   required_providers {
